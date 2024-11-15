b06499489fcc3feaa8f531043ca2b1640fabd0bf
RoLYroLLs <github@rolyrolls.com>
2024-11-15 17:12:59 -0500

    updated FluentEmail.SendGrid to .Net Standard 2.1 and formatting

--- src/Senders/FluentEmail.SendGrid/SendGridSender.cs
+++ src/Senders/FluentEmail.SendGrid/SendGridSender.cs
@@ -5,197 +5,168 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using FluentEmail.Core;
-using FluentEmail.Core.Interfaces;
 using FluentEmail.Core.Models;
 using SendGrid;
 using SendGrid.Helpers.Mail;
 using SendGridAttachment = SendGrid.Helpers.Mail.Attachment;
 
-namespace FluentEmail.SendGrid
-{
-    public class SendGridSender : ISendGridSender
-    {
-        private readonly string _apiKey;
-        private readonly bool _sandBoxMode;
+namespace FluentEmail.SendGrid;
 
-        public SendGridSender(string apiKey, bool sandBoxMode = false)
-        {
-            _apiKey = apiKey;
-            _sandBoxMode = sandBoxMode;
-        }
-        public SendResponse Send(IFluentEmail email, CancellationToken? token = null)
-        {
-            return SendAsync(email, token).GetAwaiter().GetResult();
-        }
+public class SendGridSender : ISendGridSender {
+	private readonly string _apiKey;
+	private readonly bool _sandBoxMode;
 
-        public async Task<SendResponse> SendAsync(IFluentEmail email, CancellationToken? token = null)
-        {
-            var mailMessage = await BuildSendGridMessage(email);
+	public SendGridSender(string apiKey, bool sandBoxMode = false) {
+		_apiKey = apiKey;
+		_sandBoxMode = sandBoxMode;
+	}
+	public SendResponse Send(IFluentEmail email, CancellationToken? token = null) => SendAsync(email, token).GetAwaiter().GetResult();
 
-            if (email.Data.IsHtml)
-            {
-                mailMessage.HtmlContent = email.Data.Body;
-            }
-            else
-            {
-                mailMessage.PlainTextContent = email.Data.Body;
-            }
+	public async Task<SendResponse> SendAsync(IFluentEmail email, CancellationToken? token = null) {
+		SendGridMessage mailMessage = await BuildSendGridMessage(email);
 
-            if (!string.IsNullOrEmpty(email.Data.PlaintextAlternativeBody))
-            {
-                mailMessage.PlainTextContent = email.Data.PlaintextAlternativeBody;
-            }
+		if (email.Data.IsHtml) {
+			mailMessage.HtmlContent = email.Data.Body;
+		} else {
+			mailMessage.PlainTextContent = email.Data.Body;
+		}
 
-            var sendResponse = await SendViaSendGrid(mailMessage, token);
+		if (!string.IsNullOrEmpty(email.Data.PlaintextAlternativeBody)) {
+			mailMessage.PlainTextContent = email.Data.PlaintextAlternativeBody;
+		}
 
-            return sendResponse;
-        }
+		SendResponse sendResponse = await SendViaSendGrid(mailMessage, token);
 
-        public async Task<SendResponse> SendWithTemplateAsync(IFluentEmail email, string templateId, object templateData, CancellationToken? token = null)
-        {
-            var mailMessage = await BuildSendGridMessage(email);
+		return sendResponse;
+	}
 
-            mailMessage.SetTemplateId(templateId);
-            mailMessage.SetTemplateData(templateData);
+	public async Task<SendResponse> SendWithTemplateAsync(IFluentEmail email, string templateId, object templateData, CancellationToken? token = null) {
+		SendGridMessage mailMessage = await BuildSendGridMessage(email);
 
-            var sendResponse = await SendViaSendGrid(mailMessage, token);
+		mailMessage.SetTemplateId(templateId);
+		mailMessage.SetTemplateData(templateData);
 
-            return sendResponse;
-        }
+		SendResponse sendResponse = await SendViaSendGrid(mailMessage, token);
 
-        private async Task<SendGridMessage> BuildSendGridMessage(IFluentEmail email)
-        {
-            var mailMessage = new SendGridMessage();
-            mailMessage.SetSandBoxMode(_sandBoxMode);
+		return sendResponse;
+	}
 
-            mailMessage.SetFrom(ConvertAddress(email.Data.FromAddress));
+	private async Task<SendGridMessage> BuildSendGridMessage(IFluentEmail email) {
+		if (email.Data.FromAddress == null || string.IsNullOrEmpty(email.Data.FromAddress.EmailAddress)) {
+			throw new ArgumentNullException(nameof(email.Data.FromAddress));
+		}
 
-            if (email.Data.ToAddresses.Any(a => !string.IsNullOrWhiteSpace(a.EmailAddress)))
-                mailMessage.AddTos(email.Data.ToAddresses.Select(ConvertAddress).ToList());
+		SendGridMessage mailMessage = new();
+		mailMessage.SetSandBoxMode(_sandBoxMode);
 
-            if (email.Data.CcAddresses.Any(a => !string.IsNullOrWhiteSpace(a.EmailAddress)))
-                mailMessage.AddCcs(email.Data.CcAddresses.Select(ConvertAddress).ToList());
+		mailMessage.SetFrom(ConvertAddress(email.Data.FromAddress));
 
-            if (email.Data.BccAddresses.Any(a => !string.IsNullOrWhiteSpace(a.EmailAddress)))
-                mailMessage.AddBccs(email.Data.BccAddresses.Select(ConvertAddress).ToList());
+		if (email.Data.ToAddresses.Any(a => !string.IsNullOrWhiteSpace(a.EmailAddress)))
+			mailMessage.AddTos(email.Data.ToAddresses.Select(ConvertAddress).ToList());
 
-            if (email.Data.ReplyToAddresses.Any(a => !string.IsNullOrWhiteSpace(a.EmailAddress)))
-                // SendGrid does not support multiple ReplyTo addresses
-                mailMessage.SetReplyTo(email.Data.ReplyToAddresses.Select(ConvertAddress).First());
+		if (email.Data.CcAddresses.Any(a => !string.IsNullOrWhiteSpace(a.EmailAddress)))
+			mailMessage.AddCcs(email.Data.CcAddresses.Select(ConvertAddress).ToList());
 
-            mailMessage.SetSubject(email.Data.Subject);
+		if (email.Data.BccAddresses.Any(a => !string.IsNullOrWhiteSpace(a.EmailAddress)))
+			mailMessage.AddBccs(email.Data.BccAddresses.Select(ConvertAddress).ToList());
 
-            if (email.Data.Headers.Any())
-            {
-                mailMessage.AddHeaders(email.Data.Headers.ToDictionary(kvp => kvp.Key, kvp => kvp.Value));
-            }
+		if (email.Data.ReplyToAddresses.Any(a => !string.IsNullOrWhiteSpace(a.EmailAddress)))
+			// SendGrid does not support multiple ReplyTo addresses
+			mailMessage.SetReplyTo(email.Data.ReplyToAddresses.Select(ConvertAddress).First());
 
-            if(email.Data.Tags != null && email.Data.Tags.Any())
-            {
-                mailMessage.Categories = email.Data.Tags.ToList();
-            }
+		mailMessage.SetSubject(email.Data.Subject);
 
-            if (email.Data.IsHtml)
-            {
-                mailMessage.HtmlContent = email.Data.Body;
-            }
-            else
-            {
-                mailMessage.PlainTextContent = email.Data.Body;
-            }
+		if (email.Data.Headers.Any()) {
+			mailMessage.AddHeaders(email.Data.Headers.ToDictionary(kvp => kvp.Key, kvp => kvp.Value));
+		}
 
-            switch (email.Data.Priority)
-            {
-                case Priority.High:
-                    // https://stackoverflow.com/questions/23230250/set-email-priority-with-sendgrid-api
-                    mailMessage.AddHeader("Priority", "Urgent");
-                    mailMessage.AddHeader("Importance", "High");
-                    // https://docs.microsoft.com/en-us/openspecs/exchange_server_protocols/ms-oxcmail/2bb19f1b-b35e-4966-b1cb-1afd044e83ab
-                    mailMessage.AddHeader("X-Priority", "1");
-                    mailMessage.AddHeader("X-MSMail-Priority", "High");
-                    break;
+		if (email.Data.Tags.Any()) {
+			mailMessage.Categories = email.Data.Tags.ToList();
+		}
 
-                case Priority.Normal:
-                    // Do not set anything.
-                    // Leave default values. It means Normal Priority.
-                    break;
+		if (email.Data.IsHtml) {
+			mailMessage.HtmlContent = email.Data.Body;
+		} else {
+			mailMessage.PlainTextContent = email.Data.Body;
+		}
 
-                case Priority.Low:
-                    // https://stackoverflow.com/questions/23230250/set-email-priority-with-sendgrid-api
-                    mailMessage.AddHeader("Priority", "Non-Urgent");
-                    mailMessage.AddHeader("Importance", "Low");
-                    // https://docs.microsoft.com/en-us/openspecs/exchange_server_protocols/ms-oxcmail/2bb19f1b-b35e-4966-b1cb-1afd044e83ab
-                    mailMessage.AddHeader("X-Priority", "5");
-                    mailMessage.AddHeader("X-MSMail-Priority", "Low");
-                    break;
-            }
+		switch (email.Data.Priority) {
+			case Priority.High:
+				// https://stackoverflow.com/questions/23230250/set-email-priority-with-sendgrid-api
+				mailMessage.AddHeader("Priority", "Urgent");
+				mailMessage.AddHeader("Importance", "High");
+				// https://docs.microsoft.com/en-us/openspecs/exchange_server_protocols/ms-oxcmail/2bb19f1b-b35e-4966-b1cb-1afd044e83ab
+				mailMessage.AddHeader("X-Priority", "1");
+				mailMessage.AddHeader("X-MSMail-Priority", "High");
+				break;
 
-            if (email.Data.Attachments.Any())
-            {
-                foreach (var attachment in email.Data.Attachments)
-                {
-                    var sendGridAttachment = await ConvertAttachment(attachment);
-                    mailMessage.AddAttachment(sendGridAttachment.Filename, sendGridAttachment.Content,
-                        sendGridAttachment.Type, sendGridAttachment.Disposition, sendGridAttachment.ContentId);
-                }
-            }
+			case Priority.Normal:
+				// Do not set anything.
+				// Leave default values. It means Normal Priority.
+				break;
 
-            return mailMessage;
-        }
+			case Priority.Low:
+				// https://stackoverflow.com/questions/23230250/set-email-priority-with-sendgrid-api
+				mailMessage.AddHeader("Priority", "Non-Urgent");
+				mailMessage.AddHeader("Importance", "Low");
+				// https://docs.microsoft.com/en-us/openspecs/exchange_server_protocols/ms-oxcmail/2bb19f1b-b35e-4966-b1cb-1afd044e83ab
+				mailMessage.AddHeader("X-Priority", "5");
+				mailMessage.AddHeader("X-MSMail-Priority", "Low");
+				break;
+		}
 
-        private async Task<SendResponse> SendViaSendGrid(SendGridMessage mailMessage, CancellationToken? token = null)
-        {
-            var sendGridClient = new SendGridClient(_apiKey);
-            var sendGridResponse = await sendGridClient.SendEmailAsync(mailMessage, token.GetValueOrDefault());
+		if (email.Data.Attachments.Any()) {
+			foreach (Core.Models.Attachment attachment in email.Data.Attachments) {
+				SendGridAttachment sendGridAttachment = await ConvertAttachment(attachment);
+				mailMessage.AddAttachment(sendGridAttachment.Filename, sendGridAttachment.Content,
+					sendGridAttachment.Type, sendGridAttachment.Disposition, sendGridAttachment.ContentId);
+			}
+		}
 
-            var sendResponse = new SendResponse();
+		return mailMessage;
+	}
 
-            if (sendGridResponse.Headers.TryGetValues(
-                "X-Message-ID",
-                out IEnumerable<string> messageIds))
-            {
-                sendResponse.MessageId = messageIds.FirstOrDefault();
-            }
+	private async Task<SendResponse> SendViaSendGrid(SendGridMessage mailMessage, CancellationToken? token = null) {
+		SendGridClient sendGridClient = new(_apiKey);
+		Response sendGridResponse = await sendGridClient.SendEmailAsync(mailMessage, token.GetValueOrDefault());
 
-            if (IsHttpSuccess((int)sendGridResponse.StatusCode)) return sendResponse;
+		SendResponse sendResponse = new();
 
-            sendResponse.ErrorMessages.Add($"{sendGridResponse.StatusCode}");
-            var messageBodyDictionary = await sendGridResponse.DeserializeResponseBodyAsync();
+		if (sendGridResponse.Headers.TryGetValues(
+			"X-Message-ID",
+			out IEnumerable<string> messageIds)) {
+			sendResponse.MessageId = messageIds.FirstOrDefault();
+		}
 
-            if (messageBodyDictionary.ContainsKey("errors"))
-            {
-                var errors = messageBodyDictionary["errors"];
+		if (IsHttpSuccess((int)sendGridResponse.StatusCode)) return sendResponse;
 
-                foreach (var error in errors)
-                {
-                    sendResponse.ErrorMessages.Add($"{error}");
-                }
-            }
+		sendResponse.ErrorMessages.Add($"{sendGridResponse.StatusCode}");
+		Dictionary<string, dynamic> messageBodyDictionary = await sendGridResponse.DeserializeResponseBodyAsync();
 
-            return sendResponse;
-        }
+		if (messageBodyDictionary.TryGetValue("errors", out dynamic errors)) {
+			foreach (dynamic error in errors) {
+				sendResponse.ErrorMessages.Add($"{error}");
+			}
+		}
 
-        private EmailAddress ConvertAddress(Address address) => new EmailAddress(address.EmailAddress, address.Name);
+		return sendResponse;
+	}
 
-        private async Task<SendGridAttachment> ConvertAttachment(Core.Models.Attachment attachment) => new SendGridAttachment
-        {
-            Content = await GetAttachmentBase64String(attachment.Data),
-            Filename = attachment.Filename,
-            Type = attachment.ContentType
-        };
+	private EmailAddress ConvertAddress(Address address) => new(address.EmailAddress, address.Name);
 
-        private async Task<string> GetAttachmentBase64String(Stream stream)
-        {
-            using (var ms = new MemoryStream())
-            {
-                await stream.CopyToAsync(ms);
-                return Convert.ToBase64String(ms.ToArray());
-            }
-        }
+	private async Task<SendGridAttachment> ConvertAttachment(Core.Models.Attachment attachment) => attachment.Data == null
+			? throw new ArgumentNullException(nameof(attachment.Data), "Attachment data cannot be null")
+			: new SendGridAttachment {
+				Content = await GetAttachmentBase64String(attachment.Data),
+				Filename = attachment.Filename,
+				Type = attachment.ContentType
+			};
 
-        private bool IsHttpSuccess(int statusCode)
-        {
-            return statusCode >= 200 && statusCode < 300;
-        }
-    }
+	private async Task<string> GetAttachmentBase64String(Stream stream) {
+		using MemoryStream ms = new();
+		await stream.CopyToAsync(ms);
+		return Convert.ToBase64String(ms.ToArray());
+	}
+
+	private bool IsHttpSuccess(int statusCode) => statusCode is >=200 and <300;
 }

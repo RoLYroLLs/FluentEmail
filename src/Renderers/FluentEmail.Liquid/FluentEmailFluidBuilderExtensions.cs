89ae8fd27dbf710f286b8086468c380728696a52
RoLYroLLs <github@rolyrolls.com>
2024-11-15 17:10:34 -0500

    updated FluentEmail.Liquid to .Net Standard 2.1 and formatting

--- src/Renderers/FluentEmail.Liquid/FluentEmailFluidBuilderExtensions.cs
+++ src/Renderers/FluentEmail.Liquid/FluentEmailFluidBuilderExtensions.cs
@@ -6,22 +6,16 @@ using FluentEmail.Liquid;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 // ReSharper disable once CheckNamespace
-namespace Microsoft.Extensions.DependencyInjection
-{
-    public static class FluentEmailFluidBuilderExtensions
-    {
-        public static FluentEmailServicesBuilder AddLiquidRenderer(
-            this FluentEmailServicesBuilder builder,
-            Action<LiquidRendererOptions>? configure = null)
-        {
-            builder.Services.AddOptions<LiquidRendererOptions>();
-            if (configure != null)
-            {
-                builder.Services.Configure(configure);
-            }
+namespace Microsoft.Extensions.DependencyInjection;
 
-            builder.Services.TryAddSingleton<ITemplateRenderer, LiquidRenderer>();
-            return builder;
-        }
-    }
+public static class FluentEmailFluidBuilderExtensions {
+	public static FluentEmailServicesBuilder AddLiquidRenderer(this FluentEmailServicesBuilder builder, Action<LiquidRendererOptions>? configure = null) {
+		builder.Services.AddOptions<LiquidRendererOptions>();
+		if (configure != null) {
+			builder.Services.Configure(configure);
+		}
+
+		builder.Services.TryAddSingleton<ITemplateRenderer, LiquidRenderer>();
+		return builder;
+	}
 }

671ab2d301d865e876be828488b73c5c90a0f812
RoLYroLLs <github@rolyrolls.com>
2024-11-15 17:11:29 -0500

    updated FluentEmail.Graph to .Net Standard 2.1 and formatting

--- src/Senders/FluentEmail.Graph/FluentEmailGraphBuilderExtensions.cs
+++ src/Senders/FluentEmail.Graph/FluentEmailGraphBuilderExtensions.cs
@@ -2,19 +2,11 @@
 using FluentEmail.Graph;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
-namespace Microsoft.Extensions.DependencyInjection
-{
-    public static class FluentEmailGraphBuilderExtensions
-    {
-        public static FluentEmailServicesBuilder AddGraphSender(
-            this FluentEmailServicesBuilder builder,
-            string GraphEmailAppId,
-            string GraphEmailTenantId,
-            string GraphEmailSecret,
-            bool saveSentItems = false)
-        {
-            builder.Services.TryAdd(ServiceDescriptor.Scoped<ISender>(_ => new GraphSender(GraphEmailAppId, GraphEmailTenantId, GraphEmailSecret, saveSentItems)));
-            return builder;
-        }
-    }
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class FluentEmailGraphBuilderExtensions {
+	public static FluentEmailServicesBuilder AddGraphSender(this FluentEmailServicesBuilder builder, string clientId, string tenantId, string clientSecret, bool saveSentItems = false) {
+		builder.Services.TryAdd(ServiceDescriptor.Scoped<ISender>(_ => new GraphSender(clientId, tenantId, clientSecret, saveSentItems)));
+		return builder;
+	}
 }

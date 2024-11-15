239207cebe5d93400c1c842811368043455b37be
RoLYroLLs <github@rolyrolls.com>
2024-11-15 17:12:33 -0500

    updated FluentEmail.trap to .Net Standard 2.1 and formatting

--- src/Senders/FluentEmail.Mailtrap/FluentEmailMailtrapBuilderExtensions.cs
+++ src/Senders/FluentEmail.Mailtrap/FluentEmailMailtrapBuilderExtensions.cs
@@ -2,14 +2,11 @@
 using FluentEmail.Mailtrap;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
-namespace Microsoft.Extensions.DependencyInjection
-{
-    public static class FluentEmailMailtrapBuilderExtensions
-    {
-        public static FluentEmailServicesBuilder AddMailtrapSender(this FluentEmailServicesBuilder builder, string userName, string password, string host = null, int? port = null)
-        {
-            builder.Services.TryAdd(ServiceDescriptor.Scoped<ISender>(_ => new MailtrapSender(userName, password, host, port)));
-            return builder;
-        }
-    }
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class FluentEmailMailtrapBuilderExtensions {
+	public static FluentEmailServicesBuilder AddMailtrapSender(this FluentEmailServicesBuilder builder, string userName, string password, string? host = null, int? port = null) {
+		builder.Services.TryAdd(ServiceDescriptor.Scoped<ISender>(_ => new MailtrapSender(userName, password, host, port)));
+		return builder;
+	}
 }
--- src/Senders/FluentEmail.Mailtrap/MailtrapSender.cs
+++ src/Senders/FluentEmail.Mailtrap/MailtrapSender.cs
@@ -9,53 +9,50 @@ using FluentEmail.Core.Interfaces;
 using FluentEmail.Core.Models;
 using FluentEmail.Smtp;
 
-namespace FluentEmail.Mailtrap
-{
-    /// <summary>
-    /// Send emails to a Mailtrap.io inbox
-    /// </summary>
-    public class MailtrapSender : ISender, IDisposable
-    {
-        private readonly SmtpClient _smtpClient;
-        private static readonly int[] ValidPorts = {25, 465, 2525};
+namespace FluentEmail.Mailtrap;
 
-        /// <summary>
-        /// Creates a sender that uses the given Mailtrap credentials, but does not dispose it.
-        /// </summary>
-        /// <param name="userName">Username of your mailtrap.io SMTP inbox</param>
-        /// <param name="password">Password of your mailtrap.io SMTP inbox</param>
-        /// <param name="host">Host address for the Mailtrap.io SMTP inbox</param>
-        /// <param name="port">Port for the Mailtrap.io SMTP server. Accepted values are 25, 465 or 2525.</param>
-        public MailtrapSender(string userName, string password, string host = "smtp.mailtrap.io", int? port = null)
-        {
-            if (string.IsNullOrWhiteSpace(userName))
-                throw new ArgumentException("Mailtrap UserName needs to be supplied", nameof(userName));
+/// <summary>
+/// Send emails to a Mailtrap.io inbox
+/// </summary>
+public class MailtrapSender : ISender, IDisposable {
+	private readonly SmtpClient _smtpClient;
+	private static readonly int[] ValidPorts = { 25, 465, 2525 };
 
-            if (string.IsNullOrWhiteSpace(password))
-                throw new ArgumentException("Mailtrap Password needs to be supplied", nameof(password));
+	/// <summary>
+	/// Creates a sender that uses the given Mailtrap credentials, but does not dispose it.
+	/// </summary>
+	/// <param name="userName">Username of your mailtrap.io SMTP inbox</param>
+	/// <param name="password">Password of your mailtrap.io SMTP inbox</param>
+	/// <param name="host">Host address for the Mailtrap.io SMTP inbox</param>
+	/// <param name="port">Port for the Mailtrap.io SMTP server. Accepted values are 25, 465 or 2525.</param>
+	public MailtrapSender(string userName, string password, string? host = "smtp.mailtrap.io", int? port = null) {
+		if (string.IsNullOrWhiteSpace(userName))
+			throw new ArgumentException("Mailtrap UserName needs to be supplied", nameof(userName));
 
-            if (port.HasValue && !ValidPorts.Contains(port.Value))
-                throw new ArgumentException("Mailtrap Port needs to be either 25, 465 or 2525", nameof(port));
-            
-            _smtpClient = new SmtpClient(host, port.GetValueOrDefault(2525))
-            {
-                Credentials = new NetworkCredential(userName, password),
-                EnableSsl = true
-            };
-        }
-        
-        public void Dispose() => _smtpClient?.Dispose();
-        
-        public SendResponse Send(IFluentEmail email, CancellationToken? token = null)
-        {
-            var smtpSender = new SmtpSender(_smtpClient);
-            return smtpSender.Send(email, token);
-        }
+		if (string.IsNullOrWhiteSpace(password))
+			throw new ArgumentException("Mailtrap Password needs to be supplied", nameof(password));
 
-        public Task<SendResponse> SendAsync(IFluentEmail email, CancellationToken? token = null)
-        {
-            var smtpSender = new SmtpSender(_smtpClient);
-            return smtpSender.SendAsync(email, token);
-        }
-    }
+		if (string.IsNullOrWhiteSpace(host))
+			throw new ArgumentException("Mailtrap Host needs to be supplied", nameof(host));
+
+		if (port.HasValue && !ValidPorts.Contains(port.Value))
+			throw new ArgumentException("Mailtrap Port needs to be either 25, 465 or 2525", nameof(port));
+
+		_smtpClient = new SmtpClient(host, port.GetValueOrDefault(2525)) {
+			Credentials = new NetworkCredential(userName, password),
+			EnableSsl = true
+		};
+	}
+
+	public void Dispose() => _smtpClient?.Dispose();
+
+	public SendResponse Send(IFluentEmail email, CancellationToken? token = null) {
+		SmtpSender smtpSender = new(_smtpClient);
+		return smtpSender.Send(email, token);
+	}
+
+	public Task<SendResponse> SendAsync(IFluentEmail email, CancellationToken? token = null) {
+		SmtpSender smtpSender = new(_smtpClient);
+		return smtpSender.SendAsync(email, token);
+	}
 }

30b17d30945ab49eed23f2eba76dbe5e81ee7ddb
RoLYroLLs <github@rolyrolls.com>
2024-11-17 09:58:13 -0500

    bind MailgunSender to ISender in singleton scope.

--- src/Senders/FluentEmail.Mailgun/FluentEmailMailgunBuilderExtensions.cs
+++ src/Senders/FluentEmail.Mailgun/FluentEmailMailgunBuilderExtensions.cs
@@ -6,7 +6,7 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class FluentEmailMailgunBuilderExtensions {
 	public static FluentEmailServicesBuilder AddMailGunSender(this FluentEmailServicesBuilder builder, string domainName, string apiKey, MailGunRegion mailGunRegion = MailGunRegion.USA) {
-		builder.Services.TryAdd(ServiceDescriptor.Scoped<ISender>(_ => new MailgunSender(domainName, apiKey, mailGunRegion)));
+		builder.Services.TryAdd(ServiceDescriptor.Singleton<ISender>(_ => new MailgunSender(domainName, apiKey, mailGunRegion)));
 		return builder;
 	}
 }
